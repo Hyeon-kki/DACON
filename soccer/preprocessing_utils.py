3ea90d25e13c98459072eb0a8f26dc916037de77
hyeon_kki <gusrl12101210@gmail.com>
2024-04-11 21:44:58 +0900

    season experiment

--- soccer/preprocessing_utils.py
+++ soccer/preprocessing_utils.py
@@ -3,11 +3,12 @@ import warnings
 import random 
 import pandas as pd 
 import numpy as np
+
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler 
 
-
-def preprocessing(train, test):
+def preprocessing(train_x, test_x):
 
     # 아래의 feature는 훈련에 사용하지 않는다.
     # matchID season date result goals(homeTeam) goals(awayTeam) homeTeam awayTeam	
@@ -28,28 +29,37 @@ def preprocessing(train, test):
     'redCards(awayTeam)'
     ]
 
-    train['match'] = train['homeTeam'] + '-' + train['awayTeam']
-    pair_stats = train.groupby('match')[stats_columns].mean().reset_index() # match mean
+    ''' match feature create '''
+    train_x['match'] = train_x['homeTeam'] + '-' + train_x['awayTeam']
+    test_x['match'] = test_x['homeTeam'] + '-' + test_x['awayTeam']
 
-    # test_with_stats
-    test['match'] = test['homeTeam'] + '-' + test['awayTeam']
-    test_with_stats = test.merge(pair_stats, on='match', how='left')
+    ''' test feature interpolation '''
+    latest_two_season_df = train_x[train_x['season'] >= 202324]
+    pair_stats = latest_two_season_df.groupby('match')[stats_columns].mean().reset_index() 
+    test_x_with_stats = test_x.merge(pair_stats, on='match', how='left')
+
 
     # 2부리그에서 1부리그로 처음 올라온 팀은 그전에 경기기록이 없다. 따라서, 평균으로 값 대체 
-    test_with_stats.fillna(pair_stats[stats_columns].mean(), inplace=True) # pair_stats mean
-    test = test_with_stats[train.columns]
+    test_x_with_stats.fillna(pair_stats[stats_columns].mean(), inplace=True) # pair_stats mean
+    test_x = test_x_with_stats[train_x.columns]
 
     # label encoding
-    encoding_target = list(train.dtypes[train.dtypes == "object"].index)
+    encoding_target = list(train_x.dtypes[train_x.dtypes == "object"].index)
     for i in encoding_target:
         le = LabelEncoder()
-        le.fit(train[i])
-        train[i] = le.transform(train[i])
+        le.fit(train_x[i])
+        train_x[i] = le.transform(train_x[i])
         
-        # test 데이터의 새로운 카테고리에 대해 le.classes_ 배열에 추가
-        for case in np.unique(test[i]):
+        # test_x 데이터의 새로운 카테고리에 대해 le.classes_ 배열에 추가
+        for case in np.unique(test_x[i]):
             if case not in le.classes_: 
                 le.classes_ = np.append(le.classes_, case)
         
-        test[i] = le.transform(test[i])
-    return train, test
+        test_x[i] = le.transform(test_x[i])
+
+    # scaler (오히려 성능저하)
+    # scaler = StandardScaler()
+    # train_x = scaler.fit_transform(train_x)
+    # test_x = scaler.transform(test_x)
+
+    return train_x, test_x
